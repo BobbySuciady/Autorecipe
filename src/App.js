37da862b75a49a60fe0647be70a8182a1e32ad1a
Willlam <suciadywilliam@gmail.com>
2023-08-19 13:43:46 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="taskbar">
+        <h1>AutoRecipe</h1>
+      </div>
+      <div className="content">
+        <h1>Hello World</h1>
+        <p>.</p>
+      </div>
     </div>
   );
 }
