d2267688d49e56e41598967a05d806bdddad9af4
Willlam <suciadywilliam@gmail.com>
2023-08-19 17:11:16 +1000

    Added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import RecipeCard from './RecipeCard';
 
 function App() {
+  const initialRecipes = [
+    {
+      foodName: 'Pasta Carbonara',
+      ingredients: ['Spaghetti', 'Eggs', 'Bacon', 'Parmesan', 'Pepper'],
+      instructions: '...',
+    },
+    // Add more initial recipe objects here
+  ];
+
+  const [recipes, setRecipes] = useState([]);
+
+  // Load recipes from Local Storage when the component mounts
+  useEffect(() => {
+    const savedRecipes = JSON.parse(localStorage.getItem('recipes')) || initialRecipes;
+    setRecipes(savedRecipes);
+  }, []);
+
+  const addRecipe = () => {
+    const newRecipe = {
+      foodName: 'New Recipe',
+      ingredients: [],
+      instructions: '...',
+    };
+    const updatedRecipes = [...recipes, newRecipe];
+    setRecipes(updatedRecipes);
+    // Save updated recipes to Local Storage
+    localStorage.setItem('recipes', JSON.stringify(updatedRecipes));
+  };
+
+  const updateRecipe = (index, updatedRecipe) => {
+    const updatedRecipes = [...recipes];
+    updatedRecipes[index] = updatedRecipe;
+    setRecipes(updatedRecipes);
+    // Save updated recipes to Local Storage
+    localStorage.setItem('recipes', JSON.stringify(updatedRecipes));
+  };
+
+  const deleteRecipe = (index) => {
+    const updatedRecipes = [...recipes];
+    updatedRecipes.splice(index, 1); // Remove the recipe at the specified index
+    setRecipes(updatedRecipes);
+    // Save updated recipes to Local Storage
+    localStorage.setItem('recipes', JSON.stringify(updatedRecipes));
+  };
+
   return (
     <div className="App">
       <div className="taskbar">
         <h1>AutoRecipe</h1>
       </div>
       <div className="content">
-        <h1>Hello World</h1>
-        <p>.</p>
+        {recipes.map((recipe, index) => (
+          <RecipeCard
+            key={index}
+            index={index}
+            recipe={recipe}
+            updateRecipe={updateRecipe}
+            deleteRecipe={deleteRecipe} // Pass the deleteRecipe function
+          />
+        ))}
+        <button onClick={addRecipe}>Add Recipe</button>
       </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/RecipeCard.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './RecipeCard.css';
+
+const RecipeCard = ({ index, recipe, updateRecipe, deleteRecipe }) => {
+  const [editedRecipe, setEditedRecipe] = useState({ ...recipe });
+
+  const handleInputChange = (event, field) => {
+    const { value } = event.target;
+    setEditedRecipe((prevRecipe) => ({
+      ...prevRecipe,
+      [field]: value,
+    }));
+  };
+
+  const saveChanges = () => {
+    updateRecipe(index, editedRecipe);
+  };
+
+  const handleDelete = () => {
+    deleteRecipe(index);
+  };
+
+  return (
+    <div className="recipe-card">
+      <h2>{editedRecipe.foodName}</h2>
+      <div className="recipe-card-content">
+        <label>Food Name:</label>
+        <input
+          type="text"
+          value={editedRecipe.foodName}
+          onChange={(e) => handleInputChange(e, 'foodName')}
+        />
+        <label>Ingredients:</label>
+        <textarea
+          value={editedRecipe.ingredients.join('\n')}
+          onChange={(e) => handleInputChange(e, 'ingredients')}
+        />
+        <label>Instructions:</label>
+        <textarea
+          value={editedRecipe.instructions}
+          onChange={(e) => handleInputChange(e, 'instructions')}
+        />
+        <button onClick={saveChanges}>Save Changes</button>
+        <button onClick={handleDelete}>Delete Recipe</button>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
